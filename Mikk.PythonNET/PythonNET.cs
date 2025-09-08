68ad4e6d54629394577f0077837e0f4dc31f682e
Mikk155 <martimsanchez155@gmail.com>
2025-09-08 11:05:36 -0300

    Start with PythonNET API generator

---
+++ Mikk.PythonNET/PythonNET.cs
@@ -0,0 +1,100 @@
+﻿/*
+MIT License
+
+Copyright (c) 2025 Mikk155
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+namespace Mikk.PythonNET;
+
+using Mikk.Logger;
+
+/// <summary>
+/// Generate Python classes from C# classes. Purpose: Python type hints for using with PythonNET. no more no less.
+/// </summary>
+public class TypeHint
+{
+    public static readonly Logger logger = new Logger( "PythonNET Type Hints", ConsoleColor.Yellow );
+
+    private readonly Dictionary<string, string> DocStrings = new Dictionary<string, string>();
+
+    public string GetPairs()
+    {
+        System.Text.StringBuilder s = new System.Text.StringBuilder();
+
+        foreach( var kv in this.DocStrings )
+        {
+            s.AppendLine( $"{kv.Key}" );
+            s.AppendLine( $"    {kv.Value}" );
+        }
+
+        return s.ToString();
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <param name="XMLDocument">Path to a .xml file for C# summary to Python docstring</param>
+    public TypeHint( string? XMLDocument )
+    {
+        if( !string.IsNullOrEmpty( XMLDocument ) )
+        {
+            if( File.Exists( XMLDocument ) )
+            {
+                this.DocStrings = System.Xml.Linq.XDocument.Load( XMLDocument )
+                    .Descendants( "member" )
+                    .Where( m => m.Attribute( "name" ) != null && !string.IsNullOrWhiteSpace( m.Element( "summary" )?.Value ) )
+                    .ToDictionary( m => m.Attribute( "name" )!.Value, m => m.Element( "summary" )!.Value.TrimStart() );
+            }
+            else
+            {
+                TypeHint.logger.error
+                    .Write( "XMLDocument file at path \"" )
+                    .Write( XMLDocument, ConsoleColor.Green )
+                    .Write( "\" doesn't exists!" )
+                    .NewLine();
+            }
+        }
+        else
+        {
+            TypeHint.logger.info.WriteLine( "No XMLDocument specified. Python docstring will not be generated" );
+        }
+    }
+
+    public string Generate( Type type, System.Text.StringBuilder? StringBuilder = null )
+    {
+        if( StringBuilder is null )
+        {
+            StringBuilder = new System.Text.StringBuilder();
+        }
+        else
+        {
+            StringBuilder.AppendLine();
+        }
+
+        StringBuilder.AppendLine( $"class {type.Name}:" );
+
+        if( this.DocStrings.TryGetValue( $"T:{type.Name}", out string? ClassSum ) )
+        {
+            StringBuilder.AppendLine($"\t'''{ClassSum}'''");
+        }
+
+        return StringBuilder.ToString();
+    }
+}
