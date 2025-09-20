5e66d609b6511521319f6dedab4e51d818a07fcb
Mikk155 <martimsanchez155@gmail.com>
2025-09-20 16:25:27 -0300

    Convet to a static and manual context

--- Mikk.CacheManager/CacheManager.cs
+++ Mikk.CacheManager/CacheManager.cs
@@ -28,168 +28,130 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 /// <summary>
-/// Create a context of key-value pairs of data that is saved in the moment of update.
+/// Simple cache context
 /// </summary>
 public class Cache
 {
-    public const char m_ReservedKeyword = '_';
+    /// <summary>
+    /// Prefix key names for intenal operations
+    /// </summary>
+    public const char InternalPrefix = '_';
 
+    /// <summary>
+    /// Absolute path to the json file in the current cache instance
+    /// </summary>
     public readonly string FileName;
 
-    private Dictionary<string, JToken> KeyValues = new Dictionary<string, JToken>();
-
-    private static readonly List<WeakReference<Cache>> _CacheInstances_ = new List<WeakReference<Cache>>();
+    /// <summary>
+    /// Object of the current cache instance
+    /// </summary>
+    public JObject data;
 
     public Cache( string filename )
     {
         this.FileName = filename;
+        this.data = Cache.Read( this.FileName );
+        this.data[ $"{InternalPrefix}Cache.FileName" ] = this.FileName;
+    }
 
-        if( !File.Exists( this.FileName ) )
+    /// <summary>
+    /// Try write the current cache instance to its file
+    /// </summary>
+    public void Write()
+    {
+        Cache.Write( this.FileName, this.data );
+    }
+
+    ~Cache()
+    {
+        Cache.Write( this.FileName, this.data );
+    }
+
+    /// <summary>
+    /// Find the cache owner of this object and try to write the cache instance
+    /// </summary>
+    public static void Write( JObject obj )
+    {
+        while( obj.Parent is not null )
         {
-            string dir = Path.GetDirectoryName( this.FileName )!;
+            obj = (JObject)obj.Parent;
+        }
+
+        if( obj.TryGetValue( $"{Cache.InternalPrefix}Cache.FileName", out JToken? token ) && token is not null )
+        {
+            Cache.Write( token.Value<string>()!, obj );
+        }
+    }
+
+    /// <summary>
+    /// Read a cache file into a JObject.
+    /// If it doesn't exists a new one will be generated.
+    /// If fails to open a backup will be generated and a new one will be returned.
+    /// </summary>
+    /// <param name="filename">Absolute path to the json file</param>
+    /// <returns>The json object</returns>
+    public static JObject Read( string filename )
+    {
+        if( !File.Exists( filename ) )
+        {
+            string dir = Path.GetDirectoryName( filename )!;
 
             if( !Directory.Exists( dir ) )
             {
                 Directory.CreateDirectory( dir );
             }
 
-            File.WriteAllText( this.FileName, "{}" );
+            File.WriteAllText( filename, "{}" );
         }
         else
         {
             try
             {
-                JObject file_content = (JObject)JsonConvert.DeserializeObject( File.ReadAllText( this.FileName ) )!;
-
-                foreach( KeyValuePair<string, JToken?> vals in file_content )
-                {
-                    this.KeyValues[ vals.Key ] = vals.Value!;
-                }
+                return (JObject?)JsonConvert.DeserializeObject( File.ReadAllText( filename ) ) ?? throw new Exception();
             }
             catch
             {
-                string name = Path.GetFileNameWithoutExtension( this.FileName );
+                string name = Path.GetFileNameWithoutExtension( filename );
 
                 string datetime = DateTime.Now.ToString( "yyyy-MM-dd_HH.mm.ss" );
 
-                string new_name =$"{name}_backup_{datetime}{Path.GetExtension( this.FileName )}";
+                string new_name =$"{name}_backup_{datetime}{Path.GetExtension( filename )}";
 
-                File.Copy( this.FileName, Path.Combine( Path.GetDirectoryName( this.FileName )!, new_name ) );
-                File.WriteAllText( this.FileName, "{}" );
+                File.Copy( filename, Path.Combine( Path.GetDirectoryName( filename )!, new_name ) );
+                File.WriteAllText( filename, "{}" );
 
                 Console.WriteLine( $"Failed to deserialize cache file \"{name}\"" );
                 Console.WriteLine( $"Renamed file as \"{new_name}\" to prevent issues." );
             }
         }
 
-        Cache._CacheInstances_.Add( new WeakReference<Cache>(this) );
-    }
-
-    private bool IsValidKey( string key )
-    {
-        if( key[0] == Cache.m_ReservedKeyword )
-        {
-            throw new FieldAccessException( $"Key names starting with {Cache.m_ReservedKeyword} are reserved for internal operations" );
-        }
-        return false;
+        return new JObject();
     }
 
     /// <summary>
-    /// Gets a JToken value from the cache
+    /// Try write the content cache into the given filename
     /// </summary>
-    /// <param name="key">Key name in the cache</param>
-    /// <exception cref="FieldAccessException">Key name starts with "_"</exception>
-    public JToken? GetRaw( string key )
+    /// <param name="filename">Absolute path to a json file</param>
+    /// <param name="content">Object to write</param>
+    /// <returns>The given object after validation</returns>
+    public static JObject? Write( string filename, object content )
     {
-        if( this.IsValidKey( key ) && this.KeyValues.TryGetValue( key, out JToken? value ) && value is not null )
+        string dir = Path.GetDirectoryName( filename )!;
+
+        if( !Directory.Exists( dir ) )
         {
-            return value;
+            Directory.CreateDirectory( dir );
         }
 
-        return null;
-    }
+        string Serialized = JsonConvert.SerializeObject( content, Newtonsoft.Json.Formatting.Indented );
 
-    public T? Get<T>( string key )
-    {
-        return this.Get<T>( key, null );
-    }
+        JObject? validation = (JObject)JsonConvert.DeserializeObject( File.ReadAllText( filename ) )!;
 
-    /// <summary>
-    /// Gets a value from the cache
-    /// </summary>
-    /// <typeparam name="T">Internally they're just JToken</typeparam>
-    /// <param name="key">Key name in the cache</param>
-    /// <param name="default_value">Default value to set if the cache is empty</param>
-    /// <returns>The stored value if Any. default_value if none stored and store it. or default null if no default_value provided.</returns>
-    /// <exception cref="FieldAccessException">Key name starts with "_"</exception>
-    /// <exception cref="InvalidDataException">The cast type is not the same as the stored value</exception>
-    public T Get<T>( string key, object? default_value )
-    {
-        Type type;
-
-        if( this.IsValidKey( key ) && this.KeyValues.TryGetValue( key, out JToken? value ) && value is not null )
+        if( validation is not null )
         {
-            return value.Value<T>()!;
-        }
-        else if( default_value is not null )
-        {
-            this.KeyValues[ key ] = JToken.FromObject( default_value );
-            this.Store();
-            return (T)default_value;
+            File.WriteAllText( filename, Serialized );
         }
 
-        throw new InvalidDataException( $"Key '{key}' has no value and 'default_value' was null" );
-    }
-
-    /// <summary>
-    /// Sets a key-value pair to the cache
-    /// </summary>
-    /// <param name="key">Key name in the cache</param>
-    /// <param name="value">Value to update</param>
-    /// <exception cref="FieldAccessException">The key name starts with "_"</exception>
-    public void Set( string key, object value )
-    {
-        if( this.IsValidKey( key ) )
-        {
-            this.KeyValues[ key ] = JToken.FromObject( value );
-            this.Store();
-        }
-    }
-
-    /// <summary>
-    /// Store the cache manually. this is called automatically when Get/Set
-    /// </summary>
-    public void Store()
-    {
-        File.WriteAllText( this.FileName, JsonConvert.SerializeObject( this.KeyValues, Formatting.Indented ) );
-    }
-
-#if false
-    private static void GlobalStore( object? s, EventArgs e )
-    {
-        foreach( WeakReference<Cache> cache in Cache._CacheInstances_ )
-        {
-            if( cache.TryGetTarget( out Cache? instance ) && instance is not null )
-            {
-                instance.Store();
-            }
-        }
-    }
-
-    static Cache()
-    {
-        AppDomain.CurrentDomain.ProcessExit += Cache.GlobalStore;
-        Console.CancelKeyPress += Cache.GlobalStore;
-    }
-
-    ~Cache()
-    {
-        this.Store();
-    }
-#endif
-
-    public IEnumerator<KeyValuePair<string, JToken>> GetEnumerator()
-    {
-        return this.KeyValues.GetEnumerator();
+        return validation;
     }
 }
